d979f2c588cd9a13c86aeae84f956c2edad21545
finn021 <finn.burger17@gmail.com>
2022-11-16 20:06:29 +1100

    Update launch.bat

--- launch.bat
+++ launch.bat
@@ -1,4 +1,4 @@
-	@ECHO OFF
-  SET CUR_DIR=%~dp0
-  powershell -NoProfile -ExecutionPolicy Bypass -File "%CUR_DIR%\src\main.ps1"
-	EXIT
+@ECHO OFF
+SET CUR_DIR=%~dp0
+powershell -NoProfile -ExecutionPolicy Bypass -File "%CUR_DIR%\src\main.ps1"
+EXIT
